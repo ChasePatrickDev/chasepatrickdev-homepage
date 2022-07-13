67a63e14f8bc32c099dcc8187c4d73ea7b2e8cb1
chasepatrick <91498359+chasepatrick@users.noreply.github.com>
2022-07-13 16:32:28 -0400

    Added resposive navbar

--- components/navbar.js
+++ components/navbar.js
@@ -73,6 +73,33 @@ export default function Navbar(props) {
             Posts
           </LinkItem>
         </Stack>
+
+        <Box flex={1} align="right">
+          <Box ml={2} display={{ base: "inline-block", md: "none" }}>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"
+              />
+              <MenuList>
+                <NextLink href="/" passHref>
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <NextLink href="/works" passHref>
+                  <MenuItem as={Link}>Works</MenuItem>
+                </NextLink>
+                <NextLink href="/posts" passHref>
+                  <MenuItem as={Link}>Posts</MenuItem>
+                </NextLink>
+                <NextLink href="https://github.com" passHref>
+                  <MenuItem as={Link}>View Source</MenuItem>
+                </NextLink>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
       </Container>
     </Box>
   );
