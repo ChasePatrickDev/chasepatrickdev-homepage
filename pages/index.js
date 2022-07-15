02d00c6df253312d1502afbd5357ab2259853ade
chasepatrick <91498359+chasepatrick@users.noreply.github.com>
2022-07-15 17:42:21 -0400

    Added Bio Section and Animated Theme Change Button

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,15 @@ import {
   Heading,
   Image,
   useColorModeValue,
+  Link,
+  Button,
 } from "@chakra-ui/react";
 import Section from "../components/section";
+import Paragraph from "../components/paragraph";
+import NextLink from "next/link";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { BioSection, BioYear } from "../components/bio";
+
 export default function Home() {
   return (
     <Container>
@@ -47,7 +54,65 @@ export default function Home() {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <p>Paragraph</p>
+        <Paragraph>
+          My name is Chase and I a Software Developer and Engineer based in
+          Indianapolis with a passion for problem solving and building
+          solutions.the opportunity with Kenzie Academy to build my web
+          development skills as well as my current role with Circle K that
+          allows me to elevate my development to full-stack with .Net
+          development. My goal is to use my adaptability and problem-solving as
+          a software developer and engineer. My most recent project is{" "}
+          <NextLink href="/works/nextfire">
+            <Link>NextFire</Link>
+          </NextLink>
+          .
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My Portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1996</BioYear>
+          Born in Bedford, IN
+        </BioSection>
+        <BioSection>
+          <BioYear>2018</BioYear>
+          Worked at Amazon.com as a Tier 2 Logistics Associate
+        </BioSection>
+        <BioSection>
+          <BioYear>2020</BioYear>
+          Completed the Associate Front-end Developement training program at
+          Kenzie Academy and began further self-taught education
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Began working at Circlek (Contracted through Beacon Hill) as a Junior
+          Full-Stack Software Developer
+        </BioSection>
+        <BioSection>
+          <BioYear>2022</BioYear>
+          Hired on as a full-time employee at Circlek as a Junior Full-Stack
+          Software Developer
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          Interests
+        </Heading>
+        <Paragraph>
+          Software Developement, Video Games, Music, Animation, Continued
+          Learning
+        </Paragraph>
       </Section>
     </Container>
   );
