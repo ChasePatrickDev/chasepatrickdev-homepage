6ab94829a2a92e262fb3a627d84e1c17612e1fd8
chasepatrick <chasepatrickdev@gmail.com>
2022-07-26 20:34:17 -0400

    added meta card image

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -23,11 +23,17 @@ export default function Main({ children, router }) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@chasepatrickdev" />
         <meta name="twitter:creator" content="@chasepatrickdev" />
-        {/* <meta name="twitter:image" content="https://www.craftz.dog/card.png" /> */}
+        <meta
+          name="twitter:image"
+          content="https://chasepatrickdev-homepage.vercel.app/card.png"
+        />
         <meta property="og:site_name" content="Chase Patrick" />
         <meta name="og:title" content="Chase Patrick" />
         <meta property="og:type" content="website" />
-        {/* <meta property="og:image" content="https://www.craftz.dog/card.png" /> */}
+        <meta
+          property="og:image"
+          content="https://chasepatrickdev-homepage.vercel.app/card.png"
+        />
 
         <title>Chase Patrick - Homepage</title>
       </Head>
